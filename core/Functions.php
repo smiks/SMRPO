749fd9d4790aa7d8c5985be865318d79dbf22ec1
Smiks <sandi.mikus@gmail.com>
2015-04-02 17:42:55 +0200

    Upgraded core/Functions.php

--- core/Functions.php
+++ core/Functions.php
@@ -5,27 +5,36 @@ class Functions {
 	public function __construct(){
 	}
 
-
 	/*  Functions are sorted by name ASC  */
+
+	private static function cleanInput($value)
+	{
+		$replace     = array("'",'"','<','>','\\');
+		$replacement = array("&#39;",'&quot;','&lt;','&gt;','&#092;');
+		$outputVal   = str_replace($replace, $replacement, $value);
+		return $outputVal;
+	}
 
 	public static function input($type="ALL"){
 		if(strtoupper($type) == "GET"){
-			return $_GET;
+			$ret = Functions::cleanInput($_GET);
+			return $ret;
 		}
 		elseif(strtoupper($type) == "POST"){
-			return $_POST;
+			$ret = Functions::cleanInput($_POST);
+			return $ret;
 		}
 		elseif(strtoupper($type) == "ALL"){
-			return array("GET" => $_GET, "POST" => $_POST);
+			$retGet  = Functions::cleanInput($_GET);
+			$retPost = Functions::cleanInput($_POST);
+			return array("GET" => $retGet, "POST" => $retPost);
 		}
 		return array(0);
 	}
 
-
 	public static function redirect($toUrl) {
 		header("Location: {$toURL}");
 	}
-
 
 	public static function reload($toUrl, $time=0) {
 		header("refresh:{$time};url={$toUrl}"); 
