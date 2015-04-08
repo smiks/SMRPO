d4cc676efd6d956ca7efa55147655db3041b9442
Rok Sokol <rok.sokol@gmail.com>
2015-04-08 23:08:50 +0200

    Added new login page (static page)

---
+++ app/views/signInROK.view.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Kanbanize - SMRPO#6</title>        
+		<meta name="robots" content="" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+        <link rel="icon" type="image/png" href="../../static/images/favicon.png"/>
+        <link type="text/css" rel="stylesheet" href="../../static/css/loginPage.css" />
+ 
+	</head>
+    <body>
+		<table id="layout_login">
+		<tr>
+		<td>
+			<div class="center_block" id="page_login">
+				<div>
+					<h1><center><img alt="Logo" src="../../static/images/logo7_1200.png" /></center></h1>
+					<div id="login_block">
+						<form id="form_login" action="https://friunilj.kanbanize.com/ctrl_login/login" method="post" style="display:block">
+							<fieldset>
+									<label>Email</label>
+									<div class="input_holder email">
+										<input type="email" value=""  title="" name="login_email" required autofocus />
+									</div>
+									<label>Password</label>
+									<div class="input_holder password">
+										<input type="password" value=""  title="" name="login_password" required />
+									</div>
+							</fieldset>
+							<input type="submit" value="Log In" />
+					</form>
+					</div>
+
+                        
+				</div>
+			</div>
+		</td>
+		</tr>
+        </table>
+	</body>
+</html>
