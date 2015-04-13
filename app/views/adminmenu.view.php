3b4d262b7bf3190da88ea6bb0cd5ea7f503e152f
Smiks <sandi.mikus@gmail.com>
2015-04-13 20:50:39 +0200

    updated admin menu view

--- app/views/adminmenu.view.php
+++ app/views/adminmenu.view.php
@@ -1,6 +1,16 @@
 <div class="menu_bar">
 	<div id="logo">
 		<img alt="Logo" src="../../static/images/header_logo.png"/>
+		&nbsp;
+		<big>
+			<?
+			$user = new user();
+			$info = $user->userInfoByID($_SESSION['userid']);
+			$name = strtoupper($info['name']);
+			$surname = strtoupper($info['surname']);
+			echo("Welcome, {$name} {$surname} &nbsp; (<a href='?page=logout' style='color: #cc0000;'>Logout</a>)");
+			?>
+		</big>
 	</div>
 
 	<div id="menu">
@@ -19,10 +29,6 @@
 
 		<div id="menu_option" onClick="location.href='?page=deleteuser'">
 			<a href='?page=deleteuser'>Delete User</a>
-		</div>
-
-		<div id="menu_option" onClick="location.href='?page=logout'">
-			<a href='?page=logout' style='color: #cc0000;'>Logout</a>
 		</div>
 	</div>
 
