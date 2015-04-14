febc27c71a99aa4d2e89de7c4a09bd19c93ea9ab
Smiks <sandi.mikus@gmail.com>
2015-04-14 19:03:31 +0200

    Little homepage renewal

--- app/views/homepage.view.php
+++ app/views/homepage.view.php
@@ -1,5 +1,12 @@
 [include]app/views/header.view.php[/include]
 [include]app/views/menu.view.php[/include]	
-<h1>Welcome, {{myName}} {{mySurname}}</h1>
+<div id="marginCenter">
+	<div id="bigField">
+		<h2>Table</h2>
+	</div>
 
+	<div id="bigField">
+		<h3>Here comes THE table!</h3>
+	</div>
+</div>
 [include]app/views/footer.view.php[/include]
