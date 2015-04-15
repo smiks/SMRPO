5889d625227004edc5e0e8906aeafe2d370381eb
Smiks <sandi.mikus@gmail.com>
2015-04-15 16:56:30 +0200

    added view editgroupsub.view.php

---
+++ app/views/editgroupsub.view.php
@@ -0,0 +1,12 @@
+[include]app/views/header.view.php[/include]
+[include]app/views/menu.view.php[/include]	
+<div id="toCenter">
+	<div id="field_50">
+		<h2>Edit Group</h2>
+	</div>
+	<div id="field_50">
+Message: {{message}}
+</div>
+</div>
+
+[include]app/views/footer.view.php[/include]
