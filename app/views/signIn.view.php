7e89215dfd52f84978409ca47b5f2ea18c300d37
Rok Sokol <rok.sokol@gmail.com>
2015-04-30 21:49:57 +0200

    Minor css changes

--- app/views/signIn.view.php
+++ app/views/signIn.view.php
@@ -6,15 +6,15 @@
 		<td>
 			<div class="center_block" id="page_login">
 				<div>
-					<h1><center><img alt="Logo" src="../../static/images/logo7_1200.png" /></center></h1>
+					<h1><center><img alt="Logo" src="../../static/images/logo7_1200.png" /></center></h1><br>
 					<div id="login_block">
 						<form id="form_login" action="?page=login" method="post" style="display:block">
 							<fieldset>
-									<label>Email</label>
+									<label> <span style="font-size:20px"> Email</span></label>
 									<div class="input_holder email">
 										<input type="email" value=""  title="" name="login_email" required autofocus />
 									</div>
-									<label>Password</label>
+									<label><span style="font-size:20px">Password</span></label>
 									<div class="input_holder password">
 										<input type="password" value=""  title="" name="login_password" required />
 									</div>
