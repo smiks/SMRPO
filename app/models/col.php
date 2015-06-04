3e624bdd6ceada8cc6bfec7c3bcd4fed78b48305
majagr <maja.grujicm@gmail.com>
2015-06-04 12:05:40 +0200

    Edit column story

--- app/models/col.php
+++ app/models/col.php
@@ -36,4 +36,39 @@ class col extends Model{
 		$data = $db -> fetch_row($q);
 		return($data);
 	}
+
+	public function getAcceptanceTestingColumn($boardID) {
+		global $db;
+		$q = $db -> query("SELECT * FROM Col WHERE board_id = '{$boardID}' AND testing_col='1';");
+		$data = $db -> fetch_row($q);
+		return($data);
+	}
+
+	public function updateColumn($columnID, $name, $cardLimit, $priority_col, $testing_col, $boardID, $WIPViolation) {
+
+		global $db;
+
+		if($priority_col == 1) {
+			$pr = $this -> getPriorityColumn($boardID);
+			if($pr != null) {
+				$prID = $pr["column_id"];
+				$db -> query("UPDATE Col SET priority_col='0'WHERE column_id='{$prID}';");
+			}
+		}
+
+		if($testing_col == 1) {
+			$at = $this -> getAcceptanceTestingColumn($boardID);
+			if($at != null) {
+				$atID = $at["column_id"];
+				$db -> query("UPDATE Col SET testing_col='0'WHERE column_id='{$atID}';");
+			}
+		}
+
+		$db -> query("UPDATE Col SET name='{$name}', cardLimit='{$cardLimit}', priority_col='{$priority_col}', testing_col='{$testing_col}' WHERE column_id='{$columnID}';");
+
+		//TUKAJ BO TREBA DODATI ŠE ZAPIS V TABELO ZA WIP KRŠITVE
+
+		return true;
+
+	}
 }
