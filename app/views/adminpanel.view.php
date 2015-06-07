3f013da60b8aa1dd0f067ea7ecd64ce045028211
Rok Sokol <rok.sokol@gmail.com>
2015-06-07 22:39:15 +0200

    Added critical cards + AdminPanel is now more user friendly

--- app/views/adminpanel.view.php
+++ app/views/adminpanel.view.php
@@ -1,5 +1,11 @@
 [include]app/views/header.view.php[/include]
 [include]app/views/adminmenu.view.php[/include]	
 
+<center>
+	<br><div class="center_block_header"> Admin panel </div><br>
+	<div class="center_block" style="max-width:20%;">
+		<label class="cool_font"> Hello Admin! Use buttons above (top right corner) for admin functionality. </label>
+	</div>
+</center>
 
 [include]app/views/footer.view.php[/include]
